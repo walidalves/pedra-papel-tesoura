87aabf717148d6526667f1f58a0c4d985a782b35
walid <walidarnous.1@gmail.com>
2024-06-08 10:34:54 -0300

    Comentários adicionados

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,3 +1,4 @@
+//Imports and Declarations
 import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
@@ -5,11 +6,13 @@ const Game = ({ setScore }) => {
   const location = useLocation();
   const navigate = useNavigate();
   const { playerChoice } = location.state || {};
+  //States
   const [computerChoice, setComputerChoice] = useState(null);
   const [result, setResult] = useState('');
   const [playerWins, setPlayerWins] = useState(0);
   const [computerWins, setComputerWins] = useState(0);
 
+  //Computer Choice
   useEffect(() => {
     if (playerChoice) {
       const choices = ['rock', 'paper', 'scissors'];
@@ -18,7 +21,8 @@ const Game = ({ setScore }) => {
       determineWinner(playerChoice, randomChoice);
     }
   }, [playerChoice]);
-
+  
+  //Winner Function
   const determineWinner = (player, computer) => {
     if (player === computer) {
       setResult('Empate!');
@@ -36,6 +40,7 @@ const Game = ({ setScore }) => {
     }
   };
 
+  // Win Check
   useEffect(() => {
     if (playerWins === 3) {
       alert('Parabéns! Você ganhou o jogo!');
@@ -46,6 +51,7 @@ const Game = ({ setScore }) => {
     }
   }, [playerWins, computerWins]);
 
+  // General Reset
   const resetGame = () => {
     setPlayerWins(0);
     setComputerWins(0);
@@ -53,12 +59,14 @@ const Game = ({ setScore }) => {
     navigate('/');
   };
 
+  // General Play Again
   const playAgain = () => {
     setComputerChoice(null);
     setResult('');
     navigate('/');
   };
-
+  
+  //Rendering
   return (
     <div className="result">
       <p>Você escolheu: {playerChoice}</p>
